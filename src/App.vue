198f2ee8d4ed5df2295745e63663d1b4512cb2f6
petrovsa <petrovsa@ukr.net>
2024-02-13 15:17:12 +0200

    create content

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheContent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheContent from '@/components/TheContent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheContent
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+  font-family: $font-base;
+  color: $black-1000;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 25px;
+}
+
+p {
+  padding: 0;
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  padding-top: 10px;
+  font-weight: 500;
+}
+
+h1 {
+font-size: 34px;
+line-height: 46px;
+}
+h2 {
+font-size: 28px;
+line-height: 38px;
+}
+h3 {
+font-size: 24px;
+line-height: 32px;
+}
+h4 {
+  font-size: 22px;
+line-height: 28px;
+}
+
+a {
+  text-decoration: none;
+  color: $blue-500;
+  cursor: pointer;
+
+  @media (hover: hover) {
+        &:hover {
+          text-decoration: underline;
+        }
+      }
 }
 </style>
